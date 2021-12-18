47b9806c14c38d7aebc396c6a6ed0a36b054de2a
imporoutco586 <85064183+imporoutco586@users.noreply.github.com>
2021-12-18 07:21:51 -0800

    Add files via upload

---
+++ already/unic-char-property-0.9.0/target/debug/deps/unic_char_property-2a0a432f48d7147a.d
@@ -0,0 +1,16 @@
+/root/sgx/teerustpython/already/unic-char-property-0.9.0/target/debug/deps/unic_char_property-2a0a432f48d7147a.rmeta: src/lib.rs src/pkg_info.rs src/property.rs src/range_types.rs src/macros.rs src/tables.rs
+
+/root/sgx/teerustpython/already/unic-char-property-0.9.0/target/debug/deps/libunic_char_property-2a0a432f48d7147a.rlib: src/lib.rs src/pkg_info.rs src/property.rs src/range_types.rs src/macros.rs src/tables.rs
+
+/root/sgx/teerustpython/already/unic-char-property-0.9.0/target/debug/deps/unic_char_property-2a0a432f48d7147a.d: src/lib.rs src/pkg_info.rs src/property.rs src/range_types.rs src/macros.rs src/tables.rs
+
+src/lib.rs:
+src/pkg_info.rs:
+src/property.rs:
+src/range_types.rs:
+src/macros.rs:
+src/tables.rs:
+
+# env-dep:CARGO_PKG_DESCRIPTION=UNIC — Unicode Character Tools — Character Property taxonomy, contracts and build macros
+# env-dep:CARGO_PKG_NAME=unic-char-property
+# env-dep:CARGO_PKG_VERSION=0.9.0
